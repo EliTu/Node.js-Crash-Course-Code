c129734c5a761eba830adb731050f69a597c8f2d
EliTu <eliad91@gmail.com>
2019-11-29 15:44:43 +0200

    Creating a server, with an IP and port number

--- index.js
+++ index.js
@@ -16,4 +16,19 @@ const laptopData = JSON.parse(jsonFile);
 /* Creating a server */
 
 // Run the createServer against the http module:
-const server = http.createServer();
+// Add a callback function that takes a request and a response object arguments, that is will run anytime we access the server:
+const server = http.createServer((req, res) => {
+    
+    // writeHead method on the response object allows us to set up a response header with the status code and the options object:
+    res.writeHead(200, {'Content-type': 'text/html'});
+
+    // end method on the response object
+    res.end('This is the response');
+});
+
+// Set a port number, an ip for the server to listen to and an optional callback function:
+server.listen(1337, '127.0.0.1', () =>
+	console.log('Server is listening for requests')
+);
+
+/* Routing */
