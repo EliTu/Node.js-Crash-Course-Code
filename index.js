8282d73ba1c9824ad8a3499ca62a6dca57d23aa4
EliTu <eliad91@gmail.com>
2019-11-29 16:00:37 +0200

    Setting routing

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // NODE CORE MODULES:
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
-/* Fetching system files and parsing */
+/* FILE SYSTEM */
 
 // readFileSync takes the absolute path to a JSON file, and the char encoding (otherwise it will return "buffer"):
 const jsonFile = fs.readFileSync(
@@ -13,17 +14,24 @@ const jsonFile = fs.readFileSync(
 // Parse the JSON object into a JS object:
 const laptopData = JSON.parse(jsonFile);
 
-/* Creating a server */
+/* SERVER */
 
 // Run the createServer against the http module:
 // Add a callback function that takes a request and a response object arguments, that is will run anytime we access the server:
 const server = http.createServer((req, res) => {
-    
-    // writeHead method on the response object allows us to set up a response header with the status code and the options object:
-    res.writeHead(200, {'Content-type': 'text/html'});
+	/* REQUEST */
 
-    // end method on the response object
-    res.end('This is the response');
+	// Parsing the URL of the request object, add true to specify that the query will parsed to an object:
+	const query = url.parse(req.url, true);
+
+	console.log(query);
+	/* RESPONSE */
+
+	// writeHead method on the response object allows us to set up a response header with the status code and the options object:
+	res.writeHead(200, { 'Content-type': 'text/html' });
+
+	// end method on the response object that is the final response:
+	res.end('This is the response');
 });
 
 // Set a port number, an ip for the server to listen to and an optional callback function:
