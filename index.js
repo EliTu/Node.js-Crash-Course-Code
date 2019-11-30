c2a3c2cc2237dd5870e2def00ebb5f6544d95cf6
EliTu <eliad91@gmail.com>
2019-11-30 17:24:54 +0200

    Outputing template html output filled with a laptop data

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const url = require('url');
 
 /* FILE SYSTEM */
 
-// readFileSync takes the absolute path to a JSON file, and the char encoding (otherwise it will return "buffer"):
+// readFileSync takes the absolute path to a JSON file, and the char encoding (otherwise it will return "buffer"), this one works synchronously on server init:
 const jsonFile = fs.readFileSync(
 	`${__dirname}/starter/data/data.json`,
 	'utf-8'
@@ -33,31 +33,68 @@ const server = http.createServer((req, res) => {
 	/* Routing */
 	// Routing different paths by the pathName value:
 	switch (pathName) {
-	// If the pathName is products or home screen then respond with a 200 and some text:
-	case '/products':
-	case '/':
-		res.writeHead(200, { 'Content-type': 'text/html' });
-		res.end('PRODUCTS!');
-		break;
-
-	case '/laptop':
-		// Make sure the query won't exceed the amount of id's that are available in the file (5):
-		if (id < laptopData.length) {
+		// If the pathName is products or home screen then respond with a 200 and some text:
+		case '/products':
+		case '/':
 			res.writeHead(200, { 'Content-type': 'text/html' });
-			// Indicate the number of the id by the id passed in the query:
-			res.end(`Laptop #${id}`);
-		} else {
+			res.end('PRODUCTS!');
+			break;
+
+		case '/laptop':
+			// Make sure the query won't exceed the amount of id's that are available in the file (5):
+			if (id < laptopData.length) {
+				res.writeHead(200, { 'Content-type': 'text/html' });
+				// Indicate the number of the id by the id passed in the query:
+				// res.end(`Laptop #${id}`);
+
+				// Read the template file asynchronously:
+				fs.readFile(
+					`${__dirname}/templates/template-laptop.html`,
+					'utf-8',
+					(err, data) => {
+						try {
+							// data gives us access to the readFile result:
+							const laptop = laptopData[id];
+							// Replace the template placeholders with the real data:
+							let output = data.replace(
+								/{%PRODUCTNAME%}/g,
+								laptop.productName
+							);
+							output = output.replace(/{%IMAGE%}/g, laptop.image);
+							output = output.replace(/{%PRICE%}/g, laptop.price);
+							output = output.replace(
+								/{%SCREEN%}/g,
+								laptop.screen
+							);
+							output = output.replace(/{%CPU%}/g, laptop.cpu);
+							output = output.replace(
+								/{%STORAGE%}/g,
+								laptop.storage
+							);
+							output = output.replace(/{%RAM%}/g, laptop.ram);
+							output = output.replace(
+								/{%DESCRIPTION%}/g,
+								laptop.description
+							);
+							// At the end, send the template html output as the response:
+							res.end(output);
+						} catch {
+							console.log(err);
+							res.end(`Oops! an error has occurred: ${er}`);
+						}
+					}
+				);
+			} else {
+				res.writeHead(404, { 'Content-type': 'text/html' });
+				res.end('PAGE NOT FOUND!');
+			}
+			break;
+
+		// If the path does not match pathName then respond with a 404:
+		default:
 			res.writeHead(404, { 'Content-type': 'text/html' });
 			res.end('PAGE NOT FOUND!');
 			break;
-		}
-		break;
-
-		// If the path does not match pathName then respond with a 404:
-	default:
-		res.writeHead(404, { 'Content-type': 'text/html' });
-		res.end('PAGE NOT FOUND!');
-		break;
 	}
 	/* RESPONSE */
 
